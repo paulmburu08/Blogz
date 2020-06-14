831dcc912f0f74db9d33c88f3c68243d194b3c0e
paulmburu08 <paulmburu08@gmail.com>
2020-06-15 02:44:52 +0300

    add flask uploads

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from config import config_options
 
 bootstrap = Bootstrap()
@@ -9,6 +10,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
