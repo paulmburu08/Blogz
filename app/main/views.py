fe11b001682e59687d9afcaaa1619da88a41199c
paulmburu08 <paulmburu08@gmail.com>
2020-06-15 02:35:23 +0300

    add bio to user

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,11 @@
 from flask_login import login_required
+from flask import render_template
+from . import main
+
+@main.route('/')
+@login_required
+def index():
+
+    title = 'BLOGZ'
+
+    return render_template('index.html', title = title)
--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     @property
