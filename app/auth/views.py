5ef7ab3d4c7e3e366cbbcfb25d61ed11706b2795
paulmburu08 <paulmburu08@gmail.com>
2020-06-15 02:58:11 +0300

    add email message

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
+from ..email import mail_message
 from . import auth
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -13,6 +14,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html',registration_form = form, title = title)
 
