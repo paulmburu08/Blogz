5fcb50f9908c6b3501eb3ea6e0f19ec5fc85636c
paulmburu08 <paulmburu08@gmail.com>
2020-06-14 18:10:36 +0300

    add changes

---
+++ manage.py
@@ -0,0 +1,22 @@
+from app import create_app
+from flask_script import Manager,Server
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app)
+
+if __name__ == '__main__':
+    manager.run()
