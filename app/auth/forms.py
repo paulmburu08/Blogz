e4dfa2c647870ca077f9b5b6354e93af8822404e
paulmburu08 <paulmburu08@gmail.com>
2020-06-15 01:54:19 +0300

    add base html

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import ValidationError
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
