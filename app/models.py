b5cb79c0cfd89346c327b42e47a63818a1ff99af
paulmburu08 <paulmburu08@gmail.com>
2020-06-16 16:12:25 +0300

    add subscribe model

--- app/models.py
+++ app/models.py
@@ -78,3 +78,12 @@ class Comments(db.Model):
 
     def __repr__(self):
         return f'User {self.comment}'
+
+class Subscribe(db.Model):
+    __tablename__ = 'subscribe'
+
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String)
+
+    def __repr__(self):
+        return f'User {self.email}'
