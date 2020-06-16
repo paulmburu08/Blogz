215cb77a3bd0f9ddb7e9d0fa13dadf5da301e7b0
paulmburu08 <paulmburu08@gmail.com>
2020-06-17 02:10:56 +0300

    add changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,7 +31,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Blogz login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
